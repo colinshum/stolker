cc731a33d1f3cc621b6bb4fb7d7a731f160160cf
Colin Shum <11053683+colinshum@users.noreply.github.com>
2021-04-05 16:44:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Stolker
+
+## Basics
+
+Stolker is a basic Python-based CLI client to view your portfolio's holdings. It uses the Yahoo Finance API to gather information about your portfolio.
+
+## Getting Started
+
+Firstly, clone this repository:
+`git clone https://github.com/colinshum/stolker`
+
+Next, install the dependencies using:
+```
+pip3 install yfinance
+pip3 install pandas
+```
+
+In the project's root directory, create a new file called `portfolio.txt` with the following information in each line:
+`<TICKER> <SHARES> <AVG_COST [OPTIONAL]>`
+
+### Example:
+
+```
+AAPL 5 20.10
+GME 20 45.10
+MSFT 2
+```
+
+Finally, run the program with `python3 stolker.py`.
---
+++ stolker.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import yfinance as yf
+from os import path
+
+def tracker(portfolio):
+  tickers = {}
+
+  for line in portfolio:
+    temp = line.split()
+    if len(temp) > 1:
+      tickers[temp[0]] = temp[1:]
+
+  ticker_names = list(tickers.keys())  
+
+  data = yf.download(tickers = list(tickers.keys()), period='1d', interval='1m', group_by='ticker')
+  data.dropna(inplace=True)
+
+  total = 0.00
+
+  for ticker in ticker_names:
+    cur_price = data[ticker].tail(1).iloc[0].Open
+    if len(tickers[ticker]) > 0:
+      owned = float(cur_price) * float(tickers[ticker][0])
+      total += owned
+
+      print(ticker + ": ${:.2f} @ ${:.2f}/share".format(owned, cur_price))
+
+
+      if len(tickers[ticker]) > 1:
+        sign = "+"
+        avg_cost = float(tickers[ticker][0]) * float(tickers[ticker][1])
+        cur_value = cur_price * float(tickers[ticker][0])
+        pl = cur_value - avg_cost
+        # TODO: pl_percent = 0.00
+        
+        if pl < 0:
+          sign = "-"
+          pl *= -1
+
+        print("\tTotal P/L: " + sign + "$" + "{:.2f}".format(pl))
+      print("\n")
+
+  print("-----------------------------------")
+  print("Total: \t$" + "{:.2f}".format(total))
+
+if __name__ == "__main__":
+  if path.exists("./portfolio.txt"):
+    f = open("./portfolio.txt")
+    r = f.read()
+    l = r.split("\n")
+    tracker(l)
+  else:
+    print("To begin, create a file in the project directory called `portfolio.txt`.")
+    print("Use the following format on each line: <TICKER> <SHARES> <AVG COST [Optional]>")
